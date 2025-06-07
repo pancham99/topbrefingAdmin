3ab56c933d0ca1f78f24e2052f4d30751fc96e92
pancham99 <pancham.047@gmail.com>
2025-06-07 13:22:37 +0530

    add

--- src/dashboard/pages/Banner.jsx
+++ src/dashboard/pages/Banner.jsx
@@ -11,9 +11,9 @@ const Banner = () => {
     return (
         <div className='bg-white rounded-md'>
             <div className='flex justify-between p-4'>
-                <h2 className='text-xl font-medium'>news</h2>
+                <h2 className='text-xl font-medium'>Banner</h2>
                 {
-                    store.userInfo && store.userInfo.role !== "admin" && <Link className='px-3 py-[6px] bg-purple-500 rounded-md text-white hover:bg-purple-600' to='/dashboard/createBanner'>Create Banner</Link>
+                    store.userInfo && store.userInfo.role === "admin" && <Link className='px-3 py-[6px] bg-purple-500 rounded-md text-white hover:bg-purple-600' to='/dashboard/createBanner'>Create Banner</Link>
                 }
             </div>
 
--- src/dashboard/pages/CreateBanner.jsx
+++ src/dashboard/pages/CreateBanner.jsx
@@ -160,7 +160,7 @@ const added = async (e) => {
         <button
           
           disabled={loader}
-          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 disabled:opacity-50"
+          className="bg-purple-500 text-white px-4 py-2 rounded hover:bg-blue-700 disabled:opacity-50"
         >
           {loader ? "Submitting..." : "Add Banner"}
         </button>
