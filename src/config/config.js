1a0f307ff9d1902e40b75fbe1bfaf9d3cdf2bc7a
keshrirai505 <keshrirai505@gmail.com>
2026-06-30 00:01:28 +0530

    save

--- src/config/config.js
+++ src/config/config.js
@@ -14,3 +14,23 @@ if (mode === production) {
 }
 
 export { base_url };
+
+
+
+// config.js
+
+// const production = "production";
+// const development = "development";
+
+// // Change this when deploying
+// const mode = development;
+
+// let base_url = "";
+
+// if (mode === production) {
+//     base_url = "https://bakendtopbrefing.vercel.app";
+// } else {
+//     base_url = "http://localhost:5001";
+// }
+
+// export { base_url };
